5b4ecad9752a36180b098bd1a3a93deb4ee007e8
Bojan Ilic <bi4528@student.uni-lj.si>
2020-11-07 20:42:38 +0100

    spremenjen vrsti red ukazov

--- entitete/src/main/resources/sql-scripts/init-db.sql
+++ entitete/src/main/resources/sql-scripts/init-db.sql
@@ -1,5 +1,5 @@
 INSERT INTO zaposleni (ime, priimek) VALUES ('Shaquille', 'ONeal');
 INSERT INTO zaposleni (ime, priimek) VALUES ('Hakeem', 'Olajuwon');
-INSERT INTO vrata (st_vhodov, st_izhodov, zaposleni_id) VALUES (20, 11, 1);
-INSERT INTO vrata (st_vhodov, st_izhodov, zaposleni_id) VALUES (9, 18, 2);
 INSERT INTO prostor (ime_prostora, st_vrat, trenutno_oseb, omejitev_oseb) VALUES ('Trgovina', 2, 0, 10);
+INSERT INTO vrata (st_vhodov, st_izhodov, prostor_id, zaposleni_id) VALUES (20, 11, 1, 1);
+INSERT INTO vrata (st_vhodov, st_izhodov, prostor_id, zaposleni_id) VALUES (9, 18, 1, 2);
