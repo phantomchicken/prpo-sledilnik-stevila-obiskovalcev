4ec22cd20ca2e497aa98858a34b4719a2a06fdb6
phantomchicken <nino.brezac@gmail.com>
2020-12-16 19:18:49 +0100

    Swagger implementiran

--- api/src/main/java/si/fri/prpo/skupina19/sledilnik/api/SledilnikApplication.java
+++ api/src/main/java/si/fri/prpo/skupina19/sledilnik/api/SledilnikApplication.java
@@ -1,5 +1,6 @@
 package si.fri.prpo.skupina19.sledilnik.api;
 
+import javax.annotation.security.DeclareRoles;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
@@ -8,10 +9,10 @@ import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
-
+@DeclareRoles({"uporabnik","admin"})
 @OpenAPIDefinition(info = @Info(title = "Sledilnik stevila obiskovalcev API", version = "v1",
                     contact = @Contact(email = "nb9762@student.uni-lj.si"),
-                    license = @License(name= "dev"), description = "API za storitev Sledilnik stevila obiskovalcev."),
+                    license = @License(name= "Nino Brezac, Bojan Ilic"), description = "API za storitev Sledilnik stevila obiskovalcev."),
                     servers = @Server(url = "http://localhost:8080"))
 @ApplicationPath("/v1")
 public class SledilnikApplication extends Application{
